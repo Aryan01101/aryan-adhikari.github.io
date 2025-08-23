51b14df1be48d3365272dba1330832f8335c723f
Aryan Adhikari <aad9995@uni.sydney.edu.au>
2025-08-23 22:46:56 +1000

    Chore: Initialisation of all the pages with placehodler code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// This is the entry point for your JavaScript code.
+// You can add your initialization logic here.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript initialized and DOM fully loaded.');
+    // Add your initialization code below
+});
